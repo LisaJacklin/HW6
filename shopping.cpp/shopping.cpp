3d3ae2ec03587538cffa93f925881c77ee63e1e1
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-11-07 12:04:40 -0900

    shopping.cpp struct record created

--- shopping.cpp/shopping.cpp
+++ shopping.cpp/shopping.cpp
@@ -6,9 +6,25 @@
 
 #include <iostream>
 
+struct Record {
+    std::string unitName;
+    double unitPrice;
+    int units;
+
+};
+
+
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
 //https://www.tutorialspoint.com/cplusplus-program-to-store-and-display-information-using-structure
+/*
+come up with a list of at least 5 items to sell to the user
+store these values in a map<string, record> object
+Allow the user to add/remove items from the shopping cart
+allow for more than one item to be added.
+Give an option to see their current shopping cart
+ calculate total cost with accumulate() algorithm
+*/
